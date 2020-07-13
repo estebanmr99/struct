dd9f47ffaa96a68425b9323f0308a9f05b2ea633
Joel Vega <jogevego@gmail.com>
2020-07-13 01:27:20 -0600

    [+] Interfaz (error dimensiones mayores a 100)

--- res.c
+++ res.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#include <GL/glut.h>
 #include <string.h>
+#include <pthread.h>
 #include <stdbool.h>
 
-#define MAX 100000
+#define MAX 10000
 #define MAXI 10000
 
 #define NORTE 0
@@ -17,10 +19,15 @@
 
 #define SET(a,b) ((a) |= (1<<(b)))
 
+bool is_glut_initialized_ = false;
 int totalRows, totalColumns, nodes;
+int row,column;
+int a[MAX][MAX];
 
-int maze[MAX][CAR], bfsVisited[MAX],dfsVisited[MAX];;
+int maze[MAX][CAR], bfsVisited[MAX],dfsVisited[MAX];
 int inputMaze[MAXI][MAXI];
+
+struct timespec ts = {10,0};
 
 struct Path *bfsPath;
 
@@ -39,66 +46,78 @@ struct Path {
     int arrNodes[MAX];
 };
 
-struct Node {  
+
+typedef struct{
+  int oriRow;
+  int oriColumn;
+  int desRow;
+  int desColumn;
+  int timetoWait;
+} param;
+
+
+struct Node {
     struct Path* path;
-    struct Node* next; 
-}; 
+    struct Node* next;
+};
 
-  
-struct Node* newNode(struct Path *newPath) 
-{ 
-    struct Node* stackNode = (struct Node*)malloc(sizeof(struct Node)); 
-    stackNode->path = newPath; 
-    stackNode->next = NULL; 
-    return stackNode; 
-}
-  
-int isEmpty(struct Node* stack) 
+
+struct Node* newNode(struct Path *newPath)
 {
-    return !stack; 
-} 
-  
-void push(struct Node** stack, struct Path *newPath) 
-{ 
-    struct Node* stackNode = newNode(newPath); 
-    stackNode->next = *stack; 
-    *stack = stackNode; 
-} 
-  
-struct Path* pop(struct Node** stack) 
-{ 
-    if (isEmpty(*stack)) 
-        return NULL; 
-    struct Node* temp = *stack; 
-    *stack = (*stack)->next; 
-    struct Path* popped = temp->path; 
-    free(temp); 
-  
-    return popped; 
-} 
-
-// The queue, front stores the front node of LL and rear stores the 
-// last node of LL 
-struct Queue { 
-    struct Node *front, *rear; 
-}; 
-  
-
-struct Queue* createQueue() 
-{ 
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue)); 
-    q->front = q->rear = NULL; 
-    return q; 
+    struct Node* stackNode = (struct Node*)malloc(sizeof(struct Node));
+    stackNode->path = newPath;
+    stackNode->next = NULL;
+    return stackNode;
 }
-int isEmptyQ(struct Queue* queue) 
-{ 
-    return !queue; 
-}  
+
+void display();
+
+int isEmpty(struct Node* stack)
+{
+    return !stack;
+}
+
+void push(struct Node** stack, struct Path *newPath)
+{
+    struct Node* stackNode = newNode(newPath);
+    stackNode->next = *stack;
+    *stack = stackNode;
+}
+
+struct Path* pop(struct Node** stack)
+{
+    if (isEmpty(*stack))
+        return NULL;
+    struct Node* temp = *stack;
+    *stack = (*stack)->next;
+    struct Path* popped = temp->path;
+    free(temp);
+
+    return popped;
+}
+
+// The queue, front stores the front node of LL and rear stores the
+// last node of LL
+struct Queue {
+    struct Node *front, *rear;
+};
+
+
+struct Queue* createQueue()
+{
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+int isEmptyQ(struct Queue* queue)
+{
+    return !queue;
+}
 
 struct Path* copyPathBFS(){
     struct Path* path = (struct Path*)malloc(sizeof(struct Path));
     int i;
-    
+
     if (bfsPath == NULL){
         path->len = 0;
         path->lastNode = 0;
@@ -108,20 +127,20 @@ struct Path* copyPathBFS(){
 
         return path;
     }
-    
+
     path->len = bfsPath->len;
     path->lastNode = bfsPath->lastNode;
 
     for(i = 0; i < bfsPath->len; i++)
         path->arrNodes[i] = bfsPath->arrNodes[i];
-    
+
     return path;
 }
 
 struct Path* copyPath(){
     struct Path* path = (struct Path*)malloc(sizeof(struct Path));
     int i;
-    
+
     if (dfsPath == NULL){
         path->len = 0;
         path->lastNode = 0;
@@ -131,53 +150,53 @@ struct Path* copyPath(){
 
         return path;
     }
-    
+
     path->len = dfsPath->len;
     path->lastNode = dfsPath->lastNode;
 
     for(i = 0; i < dfsPath->len; i++)
         path->arrNodes[i] = dfsPath->arrNodes[i];
-    
+
     return path;
 }
-  
 
-void enQueue(struct Queue* q, struct Path* k) 
-{ 
-    struct Node* temp = newNode(k); 
-  
-   
-    if (q->rear == NULL) { 
-        q->front = q->rear = temp; 
-        return; 
-    } 
-  
-    
-    q->rear->next = temp; 
-    q->rear = temp; 
-} 
-  
 
-struct Path* deQueue(struct Queue* q) 
-{ 
-    
-    if (q->front == NULL) 
-        return NULL; 
-  
-    
-    struct Node* temp = q->front; 
+void enQueue(struct Queue* q, struct Path* k)
+{
+    struct Node* temp = newNode(k);
+
+
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+
+
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+
+struct Path* deQueue(struct Queue* q)
+{
+
+    if (q->front == NULL)
+        return NULL;
+
+
+    struct Node* temp = q->front;
     struct Path* deQueue= temp->path;
-    
-  
-    q->front = q->front->next; 
-  
- 
-    if (q->front == NULL) 
-        q->rear = NULL; 
-  
-    free(temp); 
+
+
+    q->front = q->front->next;
+
+
+    if (q->front == NULL)
+        q->rear = NULL;
+
+    free(temp);
     return deQueue;
-} 
+}
 
 void dfsToOutput(double time_taken){
     int i;
@@ -200,7 +219,7 @@ void dfsToOutput(double time_taken){
         }
 
         int actual = currentDFSPath->arrNodes[count];
-        
+
         if (actual == 1){ //este
             SET(path[count2], (bits - 1));
         } else if(actual == 2){ //oeste
@@ -209,19 +228,19 @@ void dfsToOutput(double time_taken){
             SET(path[count2], (bits));
             SET(path[count2], (bits - 1));
         } // para norte como es 00 no se hace nada
-        
+
         bits = bits - 2;
-        count++; 
+        count++;
     }
 
-    printf("Tiempo: %d min, %d segundos, %d ms\n", (int)(time_taken/60),(int)time_taken, (int)(time_taken*1000)); 
+    printf("Tiempo: %d min, %d segundos, %d ms\n", (int)(time_taken/60),(int)time_taken, (int)(time_taken*1000));
 
     printf("%d", steps);
     int stepsToPrint = (steps * 2) / 32;
     for(i = 0; i <= stepsToPrint; i++)
         printf(" %d ", path[i]);
     printf("\n");
-    
+
 }
 void bfsToOutput(double time_taken){
     int i;
@@ -244,7 +263,7 @@ void bfsToOutput(double time_taken){
         }
 
         int actual = currentBFSPath->arrNodes[count];
-        
+
         if (actual == 1){ //este
             SET(path[count2], (bits - 1));
         } else if(actual == 2){ //oeste
@@ -253,19 +272,19 @@ void bfsToOutput(double time_taken){
             SET(path[count2], (bits));
             SET(path[count2], (bits - 1));
         } // para norte como es 00 no se hace nada
-        
+
         bits = bits - 2;
-        count++; 
+        count++;
     }
 
-    printf("Tiempo de bfs: %d min, %d segundos, %d ms\n", (int)(time_taken/60),(int)time_taken, (int)(time_taken*1000)); 
+    printf("Tiempo de bfs: %d min, %d segundos, %d ms\n", (int)(time_taken/60),(int)time_taken, (int)(time_taken*1000));
 
     printf("%d", steps);
     int stepsToPrint = (steps * 2) / 32;
     for(i = 0; i <= stepsToPrint; i++)
         printf(" %d ", path[i]);
     printf("\n");
-    
+
 }
 void bfs(int node, int des)
 {
@@ -283,7 +302,6 @@ void bfs(int node, int des)
 
 
     while(!isEmptyQ(queue)){
-
         bfsPath =  deQueue(queue);
         node = bfsPath->lastNode;
 
@@ -294,7 +312,7 @@ void bfs(int node, int des)
         }
 
         for(j=0;j<CAR;j++){
-            
+
             if(!bfsVisited[maze[node][j]] && maze[node][j] != -1){
 
                 actual = maze[node][j];
@@ -326,16 +344,16 @@ void setupBFS(int ori1, int ori2, int des1, int des2){
     int i;
     for(i=0;i<nodes;i++)
         bfsVisited[i] = 0;
-    
+
     int oriNode = (ori1 * totalColumns) + ori2;
     int desNode = (des1 * totalColumns) + des2;
 
-    clock_t t; 
-    t = clock(); 
+    clock_t t;
+    t = clock();
 
     bfs(oriNode, desNode);
 
-    t = clock() - t; 
+    t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
 
     bfsToOutput(time_taken);
@@ -364,7 +382,7 @@ void dfs(int node, int des)
         }
 
         for(j=0;j<CAR;j++){
-            
+
             if(!dfsVisited[maze[node][j]] && maze[node][j] != -1){
 
                 actual = maze[node][j];
@@ -394,16 +412,16 @@ void setupDFS(int ori1, int ori2, int des1, int des2){
     int i;
     for(i=0;i<nodes;i++)
         dfsVisited[i] = 0;
-    
+
     int oriNode = (ori1 * totalColumns) + ori2;
     int desNode = (des1 * totalColumns) + des2;
 
-    clock_t t; 
-    t = clock(); 
+    clock_t t;
+    t = clock();
 
     dfs(oriNode, desNode);
 
-    t = clock() - t; 
+    t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
 
     dfsToOutput(time_taken);
@@ -472,6 +490,245 @@ void formatInputMaze()
 
 }
 
+void init()
+{
+
+glClearColor(1, 1, 1, 1);
+
+glMatrixMode(GL_PROJECTION);
+
+gluOrtho2D(0, 800, 0,600);
+}
+
+GLvoid check(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4,int i , int j){
+  if(a[i][j]==0){
+    glBegin(GL_LINE_LOOP);
+      glVertex3f(x1, y1, 0.0);
+      glVertex3f(x2, y2, 0.0);
+    glEnd();
+    glutSwapBuffers();
+    glBegin(GL_LINE_LOOP);
+      glVertex3f(x1, y1, 0.0);
+      glVertex3f(x4, y4, 0.0);
+    glEnd();
+  }
+  else if(a[i][j]==1){
+    glBegin(GL_LINE_LOOP);
+      glVertex3f(x1, y1, 0.0);
+      glVertex3f(x2, y2, 0.0);
+    glEnd();
+  }
+
+  else if(a[i][j]==2){
+    glBegin(GL_LINE_LOOP);
+      glVertex3f(x1, y1, 0.0);
+      glVertex3f(x4, y4, 0.0);
+    glEnd();
+  }
+
+
+}
+
+GLvoid drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4,int i , int j)
+{
+glLineWidth(0.5);
+glColor3f(0, 0, 0);
+if(i==0){
+  if(j==totalColumns-1){
+    glBegin(GL_LINE_LOOP);
+      glVertex3f(x2, y2, 0.0);
+      glVertex3f(x3, y3, 0.0);
+    glEnd();
+    glutSwapBuffers();
+    }
+    glBegin(GL_LINE_LOOP);
+        glVertex3f(x3, y3, 0.0);
+        glVertex3f(x4, y4, 0.0);
+    glEnd();
+    glutSwapBuffers();
+    check(x1,y1,x2,y2,x3,y3,x4,y4,i,j);
+
+  }
+  else{
+    if(j==totalColumns-1){
+      glBegin(GL_LINE_LOOP);
+        glVertex3f(x2, y2, 0.0);
+        glVertex3f(x3, y3, 0.0);
+      glEnd();
+      glutSwapBuffers();
+      check(x1,y1,x2,y2,x3,y3,x4,y4,i,j);
+    }
+    else{
+        check(x1,y1,x2,y2,x3,y3,x4,y4,i,j);
+    }
+
+  }
+
+}
+
+
+void drawSquare2(GLint x1, GLint x2, GLint y1, GLint y2)
+{
+
+glColor3f(1, 0, 0); // black color value is 0 0 0
+glutSwapBuffers();
+if(totalRows>=50 || totalColumns>=50){
+    glPointSize(2.5);
+}
+else if(10>=totalRows && 10>=totalColumns){
+  glPointSize(500);
+}
+else if(50>totalRows && 50>totalColumns){
+  glPointSize(5);
+}
+
+glBegin(GL_POINTS);
+glVertex3f(((x2-x1)/2)+x1, ((y2-y1)/2)+y1, 0.0);
+glEnd();
+}
+
+
+GLvoid showPath(int type){
+  struct Path* camino;
+  if(type == 0){
+    camino = bfsPath;
+  }
+  else
+    camino = dfsPath;
+  int nodoInicio = ((totalRows*row)+column)+row;
+  int cont = 0;
+  int i = totalRows-1;
+  while(nodoInicio>=cont+totalColumns){
+    cont = cont+totalColumns;
+    i--;
+  }
+  int j = nodoInicio - cont;
+  int n = camino->len;
+  GLfloat x;
+  GLfloat y;
+  x = ((double)800/(double)totalColumns)*(j);
+  y = ((double)600/(double)totalRows)*(i);
+  drawSquare2(x, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows,y);
+  for(int k = 0;k<n;k++){
+    glutSwapBuffers();
+    glColor3f(0.0, 1.0, 0.0);
+    if(camino->arrNodes[k]==ESTE){
+      j++;
+      GLfloat x;
+      GLfloat y;
+      x = ((double)800/(double)totalColumns)*(j);
+      y = ((double)600/(double)totalRows)*(i);
+      drawSquare2(x, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows,y);
+    }
+    else if(camino->arrNodes[k]==OESTE){
+      j--;
+      GLfloat x;
+      GLfloat y;
+      x = ((double)800/(double)totalColumns)*(j);
+      y = ((double)600/(double)totalRows)*(i);
+      drawSquare2(x, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows,y);
+
+    }
+    else if(camino->arrNodes[k]==SUR){
+      i--;
+      GLfloat x;
+      GLfloat y;
+      x = ((double)800/(double)totalColumns)*(j);
+      y = ((double)600/(double)totalRows)*(i);
+      drawSquare2(x, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows,y);
+    }
+    else if(camino->arrNodes[k]==NORTE){
+      i++;
+      GLfloat x;
+      GLfloat y;
+      x = ((double)800/(double)totalColumns)*(j);
+      y = ((double)600/(double)totalRows)*(i);
+      drawSquare2(x, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows,y);
+    }
+
+
+
+
+    }
+
+}
+
+
+void drawMaze()
+{
+glClear(GL_COLOR_BUFFER_BIT); // Clear display window
+GLfloat x = 0;
+GLfloat y = 0;
+int i = 0;
+int j = 0;
+while(i<totalRows){
+  while (j<totalColumns) {
+    drawSquare(x, y +(double)600/(double)totalRows, x + (double)800/(double)totalColumns, y +(double)600/(double)totalRows, x + (double)800/(double)totalColumns, y, x, y,i,j);
+    x += ((double)800/(double)totalColumns);
+    j++;
+  }
+  x = 0;
+  y += ((double)600/(double)totalRows);
+  j = 0;
+  i++;
+}
+// Process all OpenGL routine s as quickly as possible
+glFlush();
+}
+
+
+
+void displayBFS(){
+  drawMaze();
+  showPath(0);
+  glutSwapBuffers();
+}
+
+void displayDFS(){
+  drawMaze();
+  showPath(1);
+  glutSwapBuffers();
+}
+
+void *threadBfs(void* p)
+{
+    param* parametros = (param*) p;
+    setupBFS(parametros->oriRow,parametros->oriColumn,parametros->desRow,parametros->desColumn);
+    int j,k,f,g;
+    for (j = totalRows-1, k = 0; j >= 0; j--, k++){
+      for (f = 0, g = 0; f <totalColumns; f++, g++){
+         a[k][f] = inputMaze[j][g];
+      }
+    }
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Maze BFS");
+    init();
+    glutDisplayFunc(displayBFS);
+    return NULL;
+}
+
+void *threadDfs(void* p)
+{
+    param* parametros = (param*) p;
+    setupDFS(parametros->oriRow,parametros->oriColumn,parametros->desRow,parametros->desColumn);
+
+    int j,k,f,g;
+    for (j = totalRows-1, k = 0; j >= 0; j--, k++){
+      for (f = 0, g = 0; f <totalColumns; f++, g++){
+         a[k][f] = inputMaze[j][g];
+      }
+    }
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Maze DFS");
+    init();
+    glutDisplayFunc(displayDFS);
+
+
+    return NULL;
+}
+
 void main(int argc,char* argv[])
 {
     int oriRow = atoi(argv[1]);
@@ -479,14 +736,30 @@ void main(int argc,char* argv[])
     int desRow = atoi(argv[3]);
     int desColumn = atoi(argv[4]);
     int timetoWait = atoi(argv[5]);
-
+    row = oriRow;
+    column = oriColumn;
     read();
     nodes = totalColumns * totalRows;
     memset(maze, -1, MAX);
     formatInputMaze();
+    param* parametros = (param*)malloc(sizeof(param));
+    parametros -> oriRow = oriRow;
+    parametros -> oriColumn = oriColumn;
+    parametros -> desRow = desRow;
+    parametros -> desColumn = desColumn;
+    parametros -> timetoWait = timetoWait;
+    pthread_t thread_bfs;
+    pthread_t thread_dfs;
+    int argc1 = 1;
+    char* argv1[] = { "res"};
+    glutInit(&argc1,argv1) ;
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    pthread_create(&thread_bfs, NULL, threadBfs, parametros);
+    pthread_join(thread_bfs, NULL);
+    pthread_create(&thread_dfs, NULL, threadDfs, parametros);
+    pthread_join(thread_dfs, NULL);
+    glutMainLoop();
 
-    nodes = totalRows * totalColumns;
 
-    setupDFS(oriRow,oriColumn,desRow,desColumn);
-    setupBFS(oriRow,oriColumn,desRow,desColumn);
+
 }
